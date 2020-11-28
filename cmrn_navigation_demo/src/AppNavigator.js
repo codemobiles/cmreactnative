f333ca5af9895c61f58b09adf86023b349560dab
codemobiles <chaiyasit.t@gmail.com>
2020-11-28 13:49:19 +0700

    navigation demo - ep8 create bottom tab

--- cmrn_navigation_demo/src/AppNavigator.js
+++ cmrn_navigation_demo/src/AppNavigator.js
@@ -5,19 +5,28 @@ import {createStackNavigator} from '@react-navigation/stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import LoginScreen from './LoginScreen';
 import RegisterScreen from './RegisterScreen';
+import Tab1Screen from './Tab1Screen';
+import Tab2Screen from './Tab2Screen';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const TabScreens = (props) => {
+  return (
+    <Tab.Navigator initialRouteName="Tab1">
+      <Stack.Screen name="Tab1" component={Tab1Screen} />
+      <Stack.Screen name="Tab2" component={Tab2Screen} />
+    </Tab.Navigator>
+  );
+};
 
 const RootStack = (props) => {
   return (
     <Stack.Navigator initialRouteName="Login">
-      <Stack.Screen
-        name="Login"
-        component={LoginScreen}
-      />
+      <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="Register" component={RegisterScreen} />
     </Stack.Navigator>
   );
 };
 
-export default RootStack;
+export default TabScreens;
