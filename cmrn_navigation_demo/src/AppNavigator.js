397771951b2906dbee5ba4c9671074b6e32c012e
codemobiles <chaiyasit.t@gmail.com>
2020-11-28 13:08:18 +0700

    navigation demo - set header option style in app navigator

--- cmrn_navigation_demo/src/AppNavigator.js
+++ cmrn_navigation_demo/src/AppNavigator.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Image, TouchableOpacity} from 'react-native';
+import {Image, TouchableOpacity, Button} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
@@ -8,14 +8,38 @@ import RegisterScreen from './RegisterScreen';
 
 const Stack = createStackNavigator();
 
+const RootStack = (props) => {
+  return (
+    <Stack.Navigator initialRouteName="Login">
+      <Stack.Screen
+        name="Login"
+        component={LoginScreen}
+        options={{
+          title: 'Home',
+          headerStyle: {
+            backgroundColor: '#779CED',
+          },
+          headerTintColor: '#FFFFFF',
+          headerTitleStyle: {color: '#fff'},
+          headerBackTitle: ' ',
+          headerRight: ()=>{
+              return (<TouchableOpacity
+                activeOpacity={0.1}
+                onPress={() => alert('www.codemobiles.com')}
+                style={{padding: 10}}>
+                <Icon
+                  name="address-card"
+                  size={20}
+                  color="#fff"
+                  
+                />
+              </TouchableOpacity>)
+          }
+        }}
+      />
+      <Stack.Screen name="Register" component={RegisterScreen} />
+    </Stack.Navigator>
+  );
+};
 
-const RootStack = (props)=>{
-    return (
-        <Stack.Navigator initialRouteName="Login">
-            <Stack.Screen name="Login" component={LoginScreen} options={{title:"Home"}}/>
-            <Stack.Screen name="Register" component={RegisterScreen}/>
-        </Stack.Navigator>
-    )
-}
-
-export default RootStack
+export default RootStack;
