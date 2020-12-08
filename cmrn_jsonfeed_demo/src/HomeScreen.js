188634553144f60b07c55ee969d3473bb58a6686
codemobiles <chaiyasit.t@gmail.com>
2020-12-08 11:38:46 +0700

    add json feed workshop ep7 - feed into state

--- cmrn_jsonfeed_demo/src/HomeScreen.js
+++ cmrn_jsonfeed_demo/src/HomeScreen.js
@@ -1,30 +1,30 @@
-import React, {useEffect} from 'react'
-import { View, Text } from 'react-native'
-import axios from "axios"
+import React, {useEffect, useState} from 'react';
+import {View, Text} from 'react-native';
+import axios from 'axios';
 
-const url = "https://codemobiles.com/adhoc/youtubes/index_new.php?username=admin&password=password&type=foods"
+const url =
+  'https://codemobiles.com/adhoc/youtubes/index_new.php?username=admin&password=password&type=foods';
 
 export default function HomeScreen() {
+  const [dataArray, setDataArray] = useState([]);
+  useEffect(() => {
+    loadData();
+  }, []);
 
-    useEffect(()=>{
-      loadData()
-    },[])
-
-    loadData = async ()=>{
-        const result = await axios.get(url)
-        console.log(JSON.stringify(result.data))      
+  loadData = async () => {
+    try {
+      const result = await axios.get(url);
+      setDataArray(result.data.youtubes);
+    } catch (e) {
+      setDataArray([]);
     }
+  };
 
-    const dataArray = [
-        {title: "Angular", price: 99, id: "1"},
-        {title: "VueJS", price: 39, id: "2"},
-        {title: "RectJS", price: 19, id: "3"},
-        {title: "Flutter", price: 32, id: "4"},
-    ]
-
-    return (
-        <View style={{flex:1}}>
-            {dataArray.map(item => <Text key={item.id}>{item.title} : {item.price}</Text>)}                        
-        </View>
-    )
+  return (
+    <View style={{flex: 1}}>
+      {dataArray.map((item) => (
+        <Text key={item.id}>{item.title}</Text>
+      ))}
+    </View>
+  );
 }
