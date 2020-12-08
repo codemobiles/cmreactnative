63070b47485b1303c6a7eb4b9ff5600a50cb4c10
codemobiles <chaiyasit.t@gmail.com>
2020-12-08 11:21:18 +0700

    add json feed workshop ep5 - map to list complex collection ui

--- cmrn_jsonfeed_demo/src/HomeScreen.js
+++ cmrn_jsonfeed_demo/src/HomeScreen.js
@@ -2,9 +2,18 @@ import React from 'react'
 import { View, Text } from 'react-native'
 
 export default function HomeScreen() {
+
+
+    const dataArray = [
+        {title: "Angular", price: 99, id: "1"},
+        {title: "VueJS", price: 39, id: "2"},
+        {title: "RectJS", price: 19, id: "3"},
+        {title: "Flutter", price: 32, id: "4"},
+    ]
+
     return (
         <View style={{flex:1}}>
-            {["Angular", "VueJS", "ReactJS"].map(item => <Text key={item}>{item}</Text>)}                        
+            {dataArray.map(item => <Text key={item.id}>{item.title} : {item.price}</Text>)}                        
         </View>
     )
 }
