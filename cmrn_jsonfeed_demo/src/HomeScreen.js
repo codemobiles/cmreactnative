e750cb9f4117063323d69b4a864e2adddc994cdf
codemobiles <chaiyasit.t@gmail.com>
2020-12-08 12:06:49 +0700

    add json feed workshop ep11 - add stylesheet

--- cmrn_jsonfeed_demo/src/HomeScreen.js
+++ cmrn_jsonfeed_demo/src/HomeScreen.js
@@ -6,6 +6,7 @@ import {
   ImageBackground,
   Image,
   TouchableOpacity,
+  StyleSheet,
 } from 'react-native';
 import axios from 'axios';
 
@@ -36,7 +37,7 @@ export default function HomeScreen() {
   return (
     <ImageBackground
       source={require('./assets/img/gradient_bg.png')}
-      style={{flex: 1}}>
+      style={styles.container}>
       {/* Banner */}
       <Image
         style={{height: 120, width: '100%'}}
@@ -51,3 +52,49 @@ export default function HomeScreen() {
     </ImageBackground>
   );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+      },
+      list_header: {
+        width: '100%',
+        height: 100,
+      },
+      listCard: {
+        overflow: 'hidden',
+        flexDirection: 'column',
+        marginTop: 20,
+        marginLeft: 20,
+        marginRight: 20,
+        backgroundColor: '#FFF',
+        borderRadius: 20,
+        padding: 0,
+      },
+      listCardView: {
+        flexDirection: 'row',
+        marginBottom: 16,
+        height: 45,
+        alignItems: 'center',
+      },
+      listAvatar: {
+        width: 45,
+        height: '100%',
+        marginRight: 16,
+      },
+      listTitleSubtitleContainer: {
+        flexDirection: 'column',
+        marginRight: 16,
+        flex: 1,
+      },
+      listTitle: {
+        fontWeight: '700',
+      },
+      listSubTitle: {
+        fontWeight: '100',
+      },
+      listYoutbeImage: {
+        width: '100%',
+        height: 190,
+      },
+});
