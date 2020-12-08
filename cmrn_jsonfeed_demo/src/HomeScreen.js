4f06ffde36a85902323e67a7d2099b5e82a6f8c3
codemobiles <chaiyasit.t@gmail.com>
2020-12-08 12:09:48 +0700

    add json feed workshop ep12 - add touchableopacity

--- cmrn_jsonfeed_demo/src/HomeScreen.js
+++ cmrn_jsonfeed_demo/src/HomeScreen.js
@@ -29,9 +29,11 @@ export default function HomeScreen() {
   };
 
   renderRow = ({item, index}) => (
-    <Text>
-      {index + 1}. {item.title}
-    </Text>
+    <TouchableOpacity onPress={()=>alert(item.title)}>
+      <Text>
+        {index + 1}. {item.title}
+      </Text>
+    </TouchableOpacity>
   );
 
   return (
@@ -54,47 +56,47 @@ export default function HomeScreen() {
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-      },
-      list_header: {
-        width: '100%',
-        height: 100,
-      },
-      listCard: {
-        overflow: 'hidden',
-        flexDirection: 'column',
-        marginTop: 20,
-        marginLeft: 20,
-        marginRight: 20,
-        backgroundColor: '#FFF',
-        borderRadius: 20,
-        padding: 0,
-      },
-      listCardView: {
-        flexDirection: 'row',
-        marginBottom: 16,
-        height: 45,
-        alignItems: 'center',
-      },
-      listAvatar: {
-        width: 45,
-        height: '100%',
-        marginRight: 16,
-      },
-      listTitleSubtitleContainer: {
-        flexDirection: 'column',
-        marginRight: 16,
-        flex: 1,
-      },
-      listTitle: {
-        fontWeight: '700',
-      },
-      listSubTitle: {
-        fontWeight: '100',
-      },
-      listYoutbeImage: {
-        width: '100%',
-        height: 190,
-      },
+  container: {
+    flex: 1,
+  },
+  list_header: {
+    width: '100%',
+    height: 100,
+  },
+  listCard: {
+    overflow: 'hidden',
+    flexDirection: 'column',
+    marginTop: 20,
+    marginLeft: 20,
+    marginRight: 20,
+    backgroundColor: '#FFF',
+    borderRadius: 20,
+    padding: 0,
+  },
+  listCardView: {
+    flexDirection: 'row',
+    marginBottom: 16,
+    height: 45,
+    alignItems: 'center',
+  },
+  listAvatar: {
+    width: 45,
+    height: '100%',
+    marginRight: 16,
+  },
+  listTitleSubtitleContainer: {
+    flexDirection: 'column',
+    marginRight: 16,
+    flex: 1,
+  },
+  listTitle: {
+    fontWeight: '700',
+  },
+  listSubTitle: {
+    fontWeight: '100',
+  },
+  listYoutbeImage: {
+    width: '100%',
+    height: 190,
+  },
 });
