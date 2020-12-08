aa4a05b700d3c556d1d31d6286e55ea69c3e6dec
codemobiles <chaiyasit.t@gmail.com>
2020-12-08 11:16:55 +0700

    add json feed workshop ep4 - map to list collection ui

--- cmrn_jsonfeed_demo/src/HomeScreen.js
+++ cmrn_jsonfeed_demo/src/HomeScreen.js
@@ -3,8 +3,8 @@ import { View, Text } from 'react-native'
 
 export default function HomeScreen() {
     return (
-        <View>
-            <Text>Homescreen</Text>
+        <View style={{flex:1}}>
+            {["Angular", "VueJS", "ReactJS"].map(item => <Text key={item}>{item}</Text>)}                        
         </View>
     )
 }
