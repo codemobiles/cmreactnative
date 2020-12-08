1932cf32511a6d653b7c9a1aa922b801a188cc48
codemobiles <chaiyasit.t@gmail.com>
2020-12-08 14:46:15 +0700

    add json feed workshop ep20 - Finish Detail Screen

--- cmrn_jsonfeed_demo/src/DetailScreen.js
+++ cmrn_jsonfeed_demo/src/DetailScreen.js
@@ -1,28 +1,103 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import {View, Text, Image, StyleSheet, ImageBackground} from 'react-native';
 
 export default function DetailScreen(props) {
-
   React.useEffect(() => {
-        setNavigationOption()
-  }, [])
+    setNavigationOption();
+  }, []);
 
   setNavigationOption = () => {
     props.navigation.setOptions({
-      title: 'Detail',
+      title: props.route.params.item.title,
       headerStyle: {
         backgroundColor: '#999CED',
       },
       headerTintColor: '#FFFFFF',
       headerTitleStyle: {color: '#fff'},
       headerBackTitle: ' ',
-      
     });
   };
 
-  return (
-    <View>
-      <Text>Debug: {JSON.stringify(props.route.params.item)}</Text>
+  renderRow = (item) => (
+    <View style={styles.listCard}>
+      {/* Avatar and title, subtitle */}
+      <View style={styles.listCardView}>
+        {/* Avatar */}
+        <Image style={styles.listAvatar} source={{uri: item.avatar_image}} />
+
+        {/* Title and Subtitle */}
+        <View style={styles.listTitleSubtitleContainer}>
+          <Text style={styles.listTitle}>{item.title}</Text>
+          <Text style={styles.listSubTitle}>{item.subtitle}</Text>
+        </View>
+      </View>
+
+      {/* Youtube Image */}
+      <Image
+        source={{uri: item.youtube_image}}
+        style={styles.listYoutbeImage}
+      />
     </View>
-  )
+  );
+
+  return (
+    <ImageBackground
+      source={require('./assets/img/gradient_bg.png')}
+      style={styles.container}>
+      {renderRow(props.route.params.item)}
+
+      {/* Banner */}
+      <Image
+        style={{height: 120, width: '100%'}}
+        resizeMode="contain"
+        source={require('./assets/img/header_react_native.png')}
+      />
+    </ImageBackground>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  list_header: {
+    width: '100%',
+    height: 100,
+  },
+  listCard: {
+    overflow: 'hidden',
+    flexDirection: 'column',
+    marginTop: 20,
+    marginLeft: 20,
+    marginRight: 20,
+    backgroundColor: '#FFF',
+    borderRadius: 20,
+    padding: 0,
+  },
+  listCardView: {
+    flexDirection: 'row',
+    marginBottom: 16,
+    height: 45,
+    alignItems: 'center',
+  },
+  listAvatar: {
+    width: 45,
+    height: '100%',
+    marginRight: 16,
+  },
+  listTitleSubtitleContainer: {
+    flexDirection: 'column',
+    marginRight: 16,
+    flex: 1,
+  },
+  listTitle: {
+    fontWeight: '700',
+  },
+  listSubTitle: {
+    fontWeight: '100',
+  },
+  listYoutbeImage: {
+    width: '100%',
+    height: 190,
+  },
+});
