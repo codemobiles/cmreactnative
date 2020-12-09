4182cd1fa09480588fef70c33d4302c56ff109c0
codemobiles <chaiyasit.t@gmail.com>
2020-12-09 09:32:46 +0700

    camera workshop ep10 Add Upload Route in Backend

--- cmrn_camera_demo/backend/server.js
+++ cmrn_camera_demo/backend/server.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const app = express()
 
+const formidable = require("formidable")
+const fs = require('fs-extra');
+
 app.get("/", (req, res)=>{
     res.end("server is ready...")
 })
@@ -8,6 +11,28 @@ app.get("/", (req, res)=>{
 app.get("/test", (req, res)=>{
     res.json({result: "ok", data: req.query})
 })
+
+
+
+app.post('/uploads', (req, res) => {
+    let form = new formidable.IncomingForm();
+    form.parse(req, (error, fields, files) => {
+      // res.json({error, fields, files});
+      var newname = Date.now();
+      var oldpath = files.userfile.path;
+      var newpath =
+        __dirname +
+        '/upload/' +
+        newname.toString() +
+        '.' +
+        files.userfile.name.split('.').pop();
+  
+      fs.move(oldpath, newpath, function (err) {
+        res.json({result: 'Upload Successfully', account: fields});
+      });
+    });
+  });
+
 
 app.listen(3000, ()=>{
     console.log("server is ready..")
