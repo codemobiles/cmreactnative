c04b4962833cc76af61ed4e9f14a66d2d1695186
codemobiles <chaiyasit.t@gmail.com>
2020-12-09 09:22:42 +0700

    camera workshop ep9 Demo Express

--- cmrn_camera_demo/backend/server.js
+++ cmrn_camera_demo/backend/server.js
@@ -1,1 +1,14 @@
-console.log("777")
+const express = require("express")
+const app = express()
+
+app.get("/", (req, res)=>{
+    res.end("server is ready...")
+})
+
+app.get("/test", (req, res)=>{
+    res.json({result: "ok", data: req.query})
+})
+
+app.listen(3000, ()=>{
+    console.log("server is ready..")
+})
